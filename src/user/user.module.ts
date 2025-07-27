23282d22476715a7389c407e46d0dcd12883b101
resna1505 <resnapangestu01@gmail.com>
2025-07-27 22:09:24 +0700

    -

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,30 +1,37 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
-import { Connection, MongoDBConnection, MySQLConnection } from './connection/connection';
+import { Connection, createConnection, MongoDBConnection, MySQLConnection } from './connection/connection';
 import { mailService, MailService } from './mail/mail.service';
 import { createUserRepository, UserRepository } from './user-repository/user-repository';
 import { MemberService } from './member/member.service';
+import { ConfigService } from '@nestjs/config';
+// import { createConnection } from 'net';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, {
-    provide: Connection,
-    useClass: process.env.DATABASE == 'mysql' ? MySQLConnection : MongoDBConnection
-  }, 
-  {
-    provide: MailService,
-    useValue: mailService,
-  },
-  {
-    provide: 'EmailService',
-    useExisting: MailService,
-  },
-  {
-    provide: UserRepository,
-    useFactory: createUserRepository,
-    inject: [Connection],
-  },
-  MemberService]
+  providers: [
+    UserService,
+    {
+      provide: Connection,
+      useFactory: createConnection,
+      inject: [ConfigService],
+    }, 
+    {
+      provide: MailService,
+      useValue: mailService,
+    },
+    {
+      provide: 'EmailService',
+      useExisting: MailService,
+    },
+    {
+      provide: UserRepository,
+      useFactory: createUserRepository,
+      inject: [Connection],
+    },
+    MemberService,
+  ],
+  exports: [UserService],
 })
 export class UserModule {}
